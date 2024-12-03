dc6b2351e3b5755555e0dfd239b9d20e004588e1
omar <omarmt0992@gmail.com>
2024-12-03 09:49:05 -0400

    cambios

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { ButtonHead } from '../src/components/ButtonHead'
 import cv from "../public/cv-omar2.pdf"
 import emailjs from '@emailjs/browser';
 import React, { useRef } from 'react';
-
+import useEmblaCarousel from "embla-carousel-react";
 
 function App() {
 
@@ -12,7 +12,7 @@ function App() {
   const form = useRef();
   const [isChecked, setIsChecked] = useState(false);
   const [data, setdata] = useState({ email: '', message: '' })
-
+  const [emblaRef] = useEmblaCarousel({ loop: true });
 
   const sendEmail = (e) => {
     e.preventDefault();
@@ -48,6 +48,12 @@ function App() {
     setDarkMode((prevMode) => !prevMode);
   };
 
+  const svgs = [
+    <svg xmlns="http://www.w3.org/2000/svg" className="h-16 w-16 text-blue-500" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}><path strokeLinecap="round" strokeLinejoin="round" d="M12 14l9-5-9-5-9 5 9 5z" /><path strokeLinecap="round" strokeLinejoin="round" d="M12 14l6.16-3.422A12.083 12.083 0 0112 12a12.083 12.083 0 01-6.16 1.578L12 14z" /><path strokeLinecap="round" strokeLinejoin="round" d="M12 14l-9-5v10l9 5 9-5V9l-9 5z" /></svg>,
+    <svg xmlns="http://www.w3.org/2000/svg" className="h-16 w-16 text-green-500" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}><path strokeLinecap="round" strokeLinejoin="round" d="M19.428 15.518a2 2 0 01-2.856 2.856m-2.577-1.023a7.5 7.5 0 11.684-9.644m-3.797 8.6a4.992 4.992 0 014.59-2.015M6.5 6.5a7.5 7.5 0 109.356 11.878M12 3v3m6 6h3M9 12H6m15 0a7.5 7.5 0 11-3.944-6.5" /></svg>,
+    <svg xmlns="http://www.w3.org/2000/svg" className="h-16 w-16 text-red-500" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}><path strokeLinecap="round" strokeLinejoin="round" d="M11.049 2.927c.3-1.016 1.722-1.016 2.022 0l1.714 5.8c.3 1.017 1.33 1.674 2.372 1.674h6.054c1.055 0 1.498 1.33.647 2.024l-4.91 3.8c-.84.653-1.223 1.842-.847 2.894l1.704 5.732c.384 1.294-.998 2.294-2.002 1.612L12 20.137l-5.803 3.636c-1.004.682-2.386-.318-2.002-1.612l1.704-5.732c.377-1.052-.006-2.241-.847-2.894l-4.91-3.8c-.85-.694-.408-2.024.647-2.024h6.054c1.043 0 2.073-.657 2.372-1.674l1.714-5.8z" /></svg>,
+  ];
+
   return (
     <>
       <header className="max-w-3xl py-6 m-auto  dark:text-white relative w-full ">
@@ -60,7 +66,7 @@ function App() {
 
 
 
-        <main className=" sm:px-0 pt-25  m-auto  max-w-[600px]">
+        <main className=" sm:px-0 pt-25  m-auto  max-w-[700px]">
 
           <div className="flex items-center flex-col sm:flex-row gap-10 pt-6 pb-4 sm:py-10 ">
             <img className="w-40 h-40 animate-pulse object-cover rounded-full border-4 border-yellow-500 shadow-purple-500 shadow-md  p-1" src="https://avatars.githubusercontent.com/u/66282767?v=4" alt="" />
@@ -68,20 +74,20 @@ function App() {
               <h2 className="mb-2 text-3xl text-purple-500 font-semibold text-xl text-center sm:text-start">Omar Méndez Torres</h2>
               <p className="text-black/80 text-lg dark:text-white max-w-lg text-center sm:text-start text-balance sm:max-w-sm">A passionate Web Developer from Dominican republic  <img className="w-5 inline mb-1 ms-1" src="/dr.png" alt="" /></p>
 
-              <div className="pt-5 sm:pt-10 flex items-center gap-6 justify-center sm:justify-start">
+              <div className=" sm:pt-5 flex items-center gap-6 justify-center sm:justify-start">
                 <a target="_blank" href="https://github.com/omarmt29"><ButtonHead size='text-1xl' color={'bg-purple-400 text-white'} icon={<FaGithub />} text='Github' /></a>
                 <a target="_blank" href={cv}><ButtonHead size='text-1xl' color={'bg-orange-400 text-white'} icon={<FaUserTie />} text='Cv' /></a>
               </div>
             </div>
           </div>
 
-
+          <p className="text-black bg-black/5 dark:text-white text-center dark:bg-white/5 p-5 rounded">Desarrollador de software con más de 3 años de experiencia en proyectos web, especializado en frontend y con conocimientos en backend. He trabajado en proyectos personales y de terceros, enfrentando desafíos que han fortalecido mis habilidades técnicas y profesionales.</p>
 
 
 
 
         </main>
-       
+
         <div className="flex w-full justify-between border-t border-purple-30/40 pt-8 mt-10 dark:border-t-white/10">
           <svg className="w-[90px] " viewBox="-18.458 -22.75 191.151 191.151" xmlns="http://www.w3.org/2000/svg" fill="#000000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M-18.458 6.58h191.151v132.49H-18.458V6.58z" fill="none"></path><path d="M40.054 113.583h-5.175c-.183-8.735-.687-16.947-1.511-24.642h-.046l-7.879 24.642h-3.94l-7.832-24.642h-.045c-.581 7.388-.947 15.602-1.099 24.642H7.81c.304-10.993 1.068-21.299 2.289-30.919h6.414l7.465 22.719h.046l7.511-22.719h6.137c1.344 11.268 2.138 21.575 2.382 30.919M62.497 90.771c-2.107 11.434-4.887 19.742-8.337 24.928-2.688 3.992-5.633 5.99-8.84 5.99-.855 0-1.91-.258-3.16-.77v-2.757c.611.088 1.328.138 2.152.138 1.498 0 2.702-.412 3.62-1.238 1.098-1.006 1.647-2.137 1.647-3.388 0-.858-.428-2.612-1.282-5.268L42.618 90.77h5.084l4.076 13.19c.916 2.995 1.298 5.086 1.145 6.277 2.229-5.953 3.786-12.444 4.673-19.468h4.901v.002z" fill="#ffffff"></path><path d="M131.382 113.583h-14.7V82.664h4.945v27.113h9.755v3.806zM112.834 114.33l-5.684-2.805c.504-.414.986-.862 1.42-1.381 2.416-2.838 3.621-7.035 3.621-12.594 0-10.229-4.014-15.346-12.045-15.346-3.938 0-7.01 1.298-9.207 3.895-2.414 2.84-3.619 7.022-3.619 12.551 0 5.435 1.068 9.422 3.205 11.951 1.955 2.291 4.902 3.438 8.843 3.438 1.47 0 2.819-.18 4.048-.543l7.4 4.308 2.018-3.474zm-18.413-6.934c-1.252-2.014-1.878-5.248-1.878-9.707 0-7.785 2.365-11.682 7.1-11.682 2.475 0 4.289.932 5.449 2.792 1.25 2.017 1.879 5.222 1.879 9.619 0 7.849-2.367 11.774-7.099 11.774-2.476.001-4.29-.928-5.451-2.796M85.165 105.013c0 2.622-.962 4.773-2.884 6.458-1.924 1.678-4.504 2.519-7.737 2.519-3.024 0-5.956-.966-8.794-2.888l1.329-2.655c2.442 1.223 4.653 1.831 6.638 1.831 1.863 0 3.319-.413 4.375-1.232 1.055-.822 1.684-1.975 1.684-3.433 0-1.837-1.281-3.407-3.631-4.722-2.167-1.19-6.501-3.678-6.501-3.678-2.349-1.712-3.525-3.55-3.525-6.578 0-2.506.877-4.529 2.632-6.068 1.757-1.545 4.024-2.315 6.803-2.315 2.87 0 5.479.769 7.829 2.291l-1.192 2.656c-2.01-.854-3.994-1.281-5.951-1.281-1.585 0-2.809.381-3.66 1.146-.858.762-1.387 1.737-1.387 2.933 0 1.828 1.308 3.418 3.722 4.759 2.196 1.192 6.638 3.723 6.638 3.723 2.409 1.709 3.612 3.53 3.612 6.534" fill="#f8981d"></path><path d="M137.59 72.308c-2.99-.076-5.305.225-7.248 1.047-.561.224-1.453.224-1.531.933.303.3.338.784.601 1.198.448.747 1.229 1.752 1.942 2.276.783.6 1.569 1.194 2.393 1.717 1.453.899 3.1 1.422 4.516 2.318.825.521 1.645 1.195 2.471 1.756.406.299.666.784 1.193.971v-.114c-.264-.336-.339-.822-.598-1.196l-1.122-1.082c-1.084-1.456-2.431-2.727-3.884-3.771-1.196-.824-3.812-1.944-4.297-3.322l-.076-.076c.822-.077 1.797-.375 2.578-.604 1.271-.335 2.43-.259 3.734-.594.6-.15 1.195-.338 1.797-.523v-.337c-.676-.673-1.158-1.567-1.869-2.203-1.902-1.643-3.998-3.25-6.164-4.595-1.16-.749-2.652-1.231-3.887-1.868-.445-.225-1.195-.336-1.457-.71-.67-.822-1.047-1.904-1.533-2.877-1.08-2.053-2.129-4.331-3.061-6.502-.674-1.456-1.084-2.91-1.906-4.257-3.85-6.35-8.031-10.196-14.457-13.971-1.381-.786-3.024-1.121-4.779-1.533l-2.803-.148c-.598-.262-1.197-.973-1.719-1.309-2.132-1.344-7.621-4.257-9.189-.411-1.01 2.431 1.494 4.821 2.354 6.054.635.856 1.458 1.83 1.902 2.802.263.635.337 1.309.6 1.98.598 1.644 1.157 3.473 1.943 5.007.41.782.857 1.604 1.381 2.312.3.414.822.597.936 1.272-.521.744-.562 1.867-.861 2.801-1.344 4.221-.819 9.45 1.086 12.552.596.934 2.018 2.99 3.92 2.202 1.684-.672 1.311-2.801 1.795-4.668.111-.451.038-.747.262-1.043v.073c.521 1.045 1.047 2.052 1.53 3.1 1.159 1.829 3.177 3.735 4.858 5.002.895.676 1.604 1.832 2.725 2.245V74.1h-.074c-.227-.335-.559-.485-.857-.745-.674-.673-1.42-1.495-1.943-2.241-1.566-2.093-2.952-4.41-4.182-6.801-.602-1.16-1.121-2.428-1.606-3.586-.226-.447-.226-1.121-.601-1.346-.562.821-1.381 1.532-1.791 2.538-.711 1.609-.785 3.588-1.049 5.646l-.147.072c-1.19-.299-1.604-1.53-2.056-2.575-1.119-2.654-1.307-6.914-.336-9.976.26-.783 1.385-3.249.936-3.995-.225-.715-.973-1.122-1.383-1.685-.482-.708-1.01-1.604-1.346-2.39-.896-2.091-1.347-4.408-2.312-6.498-.451-.974-1.234-1.982-1.868-2.879-.712-1.008-1.495-1.718-2.058-2.913-.186-.411-.447-1.083-.148-1.53.073-.3.225-.412.523-.487.484-.409 1.867.111 2.352.336 1.385.56 2.543 1.083 3.699 1.867.523.375 1.084 1.085 1.755 1.272h.786c1.193.26 2.538.072 3.661.41 1.979.636 3.772 1.569 5.38 2.576 4.893 3.103 8.928 7.512 11.652 12.778.447.858.637 1.644 1.045 2.539.787 1.832 1.76 3.7 2.541 5.493.785 1.755 1.533 3.547 2.654 5.005.559.784 2.805 1.195 3.812 1.606.745.335 1.905.633 2.577 1.044 1.271.783 2.537 1.682 3.732 2.543.595.448 2.465 1.382 2.576 2.13M99.484 39.844a5.82 5.82 0 0 0-1.529.188v.075h.072c.301.597.824 1.011 1.197 1.532.301.599.562 1.193.857 1.791l.072-.074c.527-.373.789-.971.789-1.868-.227-.264-.262-.522-.451-.784-.22-.374-.705-.56-1.007-.86" fill="#ffffff"></path><path d="M141.148 113.578h.774v-3.788h-1.161l-.947 2.585-1.029-2.585h-1.118v3.788h.731v-2.882h.041l1.078 2.882h.557l1.074-2.882v2.882zm-6.235 0h.819v-3.146h1.072v-.643h-3.008v.643h1.115l.002 3.146z" fill="#f8981d"></path></g></svg>
           <svg className="w-[60px] " fill="#44a8b3" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" xml:space="preserve"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path fill-rule="evenodd" clip-rule="evenodd" d="M12 6.036c-2.667 0-4.333 1.325-5 3.976 1-1.325 2.167-1.822 3.5-1.491.761.189 1.305.738 1.906 1.345C13.387 10.855 14.522 12 17 12c2.667 0 4.333-1.325 5-3.976-1 1.325-2.166 1.822-3.5 1.491-.761-.189-1.305-.738-1.907-1.345-.98-.99-2.114-2.134-4.593-2.134zM7 12c-2.667 0-4.333 1.325-5 3.976 1-1.326 2.167-1.822 3.5-1.491.761.189 1.305.738 1.907 1.345.98.989 2.115 2.134 4.594 2.134 2.667 0 4.333-1.325 5-3.976-1 1.325-2.167 1.822-3.5 1.491-.761-.189-1.305-.738-1.906-1.345C10.613 13.145 9.478 12 7 12z"></path></g></svg>
@@ -109,14 +115,24 @@ function App() {
           <div className="col-span-2 md:col-span-1">
             <div className="mt-8 gap-16 grid grid-cols-3 ">
 
+              <div className="col-span-3 sm:col-span-3 flex-col sm:flex-row flex">
+                <div className="w-full h-full sm:h-40">
+                  <img className=" rounded-md object-cover w-full h-full   border-2 border-yellow-500 shadow-purple-500 shadow-md  p-1" src="/salvamentosrd.png" alt="" />
+                </div>
+                <div className="sm:pl-4 w-full mt-4 sm:mt-0">
+                  <a href="https://salvamentosrd.com/" target="_blank"> <h3 className="dark:text-white font-semibold text-xl mb-2">SalvamentosRD</h3> </a>
+                  <p className="dark:text-white/60 text-[0.9rem]">Salvamento RD es líder en la venta de vehículos, salvamentos y piezas de repuesto.</p>
+                  <a href="https://salvamentosrd.com/" target="_blank"> <ButtonHead color='float-right sm:float-none bg-purple-400 text-white px-3 mt-4 py-1' size='sm:text-[0.9rem]' text='Live' icon={<FaLink />} /></a>
+                </div>
+              </div>
 
               <div className="col-span-3 sm:col-span-3 flex-col sm:flex-row flex">
                 <div className="w-full h-full sm:h-40">
                   <img className=" rounded-md object-cover w-full h-full   border-2 border-yellow-500 shadow-purple-500 shadow-md  p-1" src="/canino.png" alt="" />
                 </div>
                 <div className="sm:pl-4 w-full mt-4 sm:mt-0">
-                  <h3 className="dark:text-white font-semibold text-xl">Il Canino Pet Spa</h3>
-                  <p className="dark:text-white/60 text-[0.9rem]">Il Canino se compromete a ofrecer servicios personalizados de peluquería y veterinaria, promoviendo la salud y el bienestar de nuestros amigos de cuatro patas.</p>
+                  <h3 className="dark:text-white font-semibold text-xl mb-2 ">Il Canino Pet Spa</h3>
+                  <p className="dark:text-white/60 text-[0.9rem]">Il Canino se compromete a ofrecer servicios personalizados de peluquería y veterinaria.</p>
                   <a href="https://ilcaninopetspa.com/" target="_blank"> <ButtonHead color='float-right sm:float-none bg-purple-400 text-white px-3 mt-4 py-1' size='sm:text-[0.9rem]' text='Live' icon={<FaLink />} /></a>
                 </div>
               </div>
@@ -126,7 +142,7 @@ function App() {
                   <img className=" rounded-md object-cover w-full h-full   border-2 border-yellow-500 shadow-purple-500 shadow-md  p-1" src="/training.png" alt="" />
                 </div>
                 <div className="sm:pl-4 w-full mt-4 sm:mt-0">
-                  <h3 className="dark:text-white font-semibold text-xl">Training With Stella</h3>
+                  <h3 className="dark:text-white font-semibold text-xl mb-2 ">Training With Stella</h3>
                   <p className="dark:text-white/60 text-[0.9rem]">Entrena a tu perro hoy para disfrutar de una vida juntos llena de felicidad</p>
                   <a href="https://trainingwithstella.com/" target="_blank"> <ButtonHead color='float-right sm:float-none bg-purple-400 text-white px-3 mt-4 py-1' size='sm:text-[0.9rem]' text='Live' icon={<FaLink />} /></a>
                 </div>
@@ -137,7 +153,7 @@ function App() {
                   <img className=" rounded-md object-cover w-full h-full   border-2 border-yellow-500 shadow-purple-500 shadow-md  p-1" src="/noval.png" alt="" />
                 </div>
                 <div className="sm:pl-4 w-full mt-4 sm:mt-0">
-                  <h3 className="dark:text-white font-semibold text-xl">Noval Properties</h3>
+                  <h3 className="dark:text-white font-semibold text-xl mb-2 ">Noval Properties</h3>
                   <p className="dark:text-white/60 text-[0.9rem]">It's a corporate website for a real estate developer</p>
                   <div className="flex gap-2 mb-2 mt-3">
                     <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/PHP-logo.svg/2560px-PHP-logo.svg.png" className="w-10 h-10 object-contain" alt="" />
@@ -155,7 +171,7 @@ function App() {
                   <img className=" rounded-md object-cover w-full h-full   border-2 border-yellow-500 shadow-purple-500 shadow-md  p-1" src="/academy.png" alt="" />
                 </div>
                 <div className="sm:pl-4 w-full mt-4 sm:mt-0">
-                  <h3 className="dark:text-white font-semibold text-xl">Noval Academy</h3>
+                  <h3 className="dark:text-white font-semibold text-xl mb-2 ">Noval Academy</h3>
                   <p className="dark:text-white/60 text-[0.9rem]">Landing page for an event with a variety of functions from parallax scrolling, animations, etc.</p>
                   <div className="flex gap-2 mb-2 mt-3">
                     <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/PHP-logo.svg/2560px-PHP-logo.svg.png" className="w-10 h-10 object-contain" alt="" />
@@ -174,7 +190,7 @@ function App() {
                   <img className=" rounded-md object-cover w-full h-full  border-2 border-yellow-500 shadow-purple-500 shadow-md  p-1" src="/turistapp3.png" alt="" />
                 </div>
                 <div className="sm:pl-4 w-full mt-4 sm:mt-0">
-                  <h3 className="dark:text-white font-semibold text-xl">TuristApp</h3>
+                  <h3 className="dark:text-white font-semibold text-xl mb-2 ">TuristApp</h3>
                   <p className="dark:text-white/60 text-[0.9rem]">It's a web application for managing tourist activities, clients, and schedules</p>
                   <div className="flex items-end gap-2 mb-4 mt-5">
                     <img src="/reactjs.png" className="w-18 h-8 object-contain" alt="" />
@@ -202,7 +218,7 @@ function App() {
                   <img className=" rounded-md object-cover w-full h-full   border-2 border-yellow-500 shadow-purple-500 shadow-md  p-1" src="/events.png" alt="" />
                 </div>
                 <div className="sm:pl-4 w-full mt-4 sm:mt-0">
-                  <h3 className="dark:text-white font-semibold text-xl">Mercado Events</h3>
+                  <h3 className="dark:text-white font-semibold text-xl mb-2 ">Mercado Events</h3>
                   <p className="dark:text-white/60 text-[0.9rem]">Organizamos una gran variedad de rankings, premios y summits alrededor del año</p>
                   <a href="https://revistamercado.do/events/" target="_blank"> <ButtonHead color='float-right sm:float-none bg-purple-400 text-white px-3 mt-4 py-1' size='sm:text-[0.9rem]' text='Live' icon={<FaLink />} /></a>
                 </div>
@@ -213,7 +229,7 @@ function App() {
                   <img className=" rounded-md object-cover w-full h-full   border-2 border-yellow-500 shadow-purple-500 shadow-md  p-1" src="/tbodr.png" alt="" />
                 </div>
                 <div className="sm:pl-4 w-full mt-4 sm:mt-0">
-                  <h3 className="dark:text-white font-semibold text-xl">The best of dr</h3>
+                  <h3 className="dark:text-white font-semibold text-xl mb-2 ">The best of dr</h3>
                   <p className="dark:text-white/60 text-[0.9rem]">Te ofrecemos una selección especial con los tesoros más destacados de todo el país.</p>
                   <a href="https://thebestofdr.do/" target="_blank"> <ButtonHead color='float-right sm:float-none bg-purple-400 text-white px-3 mt-4 py-1' size='sm:text-[0.9rem]' text='Live' icon={<FaLink />} /></a>
                 </div>
@@ -223,7 +239,7 @@ function App() {
                   <img className=" rounded-md object-cover w-full h-full   border-2 border-yellow-500 shadow-purple-500 shadow-md  p-1" src="/winners.png" alt="" />
                 </div>
                 <div className="sm:pl-4 w-full mt-4 sm:mt-0">
-                  <h3 className="dark:text-white font-semibold text-xl">Winners 2024</h3>
+                  <h3 className="dark:text-white font-semibold text-xl mb-2 ">Winners 2024</h3>
                   <p className="dark:text-white/60 text-[0.9rem]">Cada año disfrutamos de una selección de los personajes y proyectos que nos hacen sentir orgullosos de ser dominicanos.</p>
                   <a href="https://thebestofdr.do/winner-2024/" target="_blank"> <ButtonHead color='float-right sm:float-none bg-purple-400 text-white px-3 mt-4 py-1' size='sm:text-[0.9rem]' text='Live' icon={<FaLink />} /></a>
                 </div>
@@ -233,7 +249,7 @@ function App() {
                   <img className=" rounded-md object-cover w-full h-full   border-2 border-yellow-500 shadow-purple-500 shadow-md  p-1" src="/elitehub.png" alt="" />
                 </div>
                 <div className="sm:pl-4 w-full mt-4 sm:mt-0">
-                  <h3 className="dark:text-white font-semibold text-xl">EliteHub</h3>
+                  <h3 className="dark:text-white font-semibold text-xl mb-2 ">EliteHub</h3>
                   <p className="dark:text-white/60 text-[0.9rem]">Somos la plataforma que conecta a tu marca con una audiencia comprometida.</p>
                   <a href="https://revistamercado.do/elitehub/" target="_blank"> <ButtonHead color='float-right sm:float-none bg-purple-400 text-white px-3 mt-4 py-1' size='sm:text-[0.9rem]' text='Live' icon={<FaLink />} /></a>
                 </div>
